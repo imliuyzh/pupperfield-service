6c6dafdff3985927c7cec21048c4f09e2edf8567
Yizhen Liu <imliuyzh@outlook.com>
2025-05-09 11:13:56 -0700

    Set up proper connection pooling

--- src/main/java/com/pupperfield/backend/config/DatabaseConfig.java
+++ src/main/java/com/pupperfield/backend/config/DatabaseConfig.java
@@ -1,10 +1,10 @@
 package com.pupperfield.backend.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 import lombok.AllArgsConstructor;
 
@@ -12,10 +12,10 @@ import lombok.AllArgsConstructor;
 @Configuration
 public class DatabaseConfig {
     @Bean
-    DataSource dataSource() {
-        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.sqlite.JDBC");
-        dataSource.setUrl("jdbc:sqlite:src/main/resources/database/dogs.db");
-        return dataSource;
+    HikariDataSource dataSource() {
+        var config = new HikariConfig();
+        config.setJdbcUrl("jdbc:sqlite:src/main/resources/database/dogs.db");
+        config.setPoolName("pupperfield");
+        return new HikariDataSource(config);
     }
 }
