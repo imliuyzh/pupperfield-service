ce18c419ed4f91b6a49c3571fcdf2692670e4a54
Yizhen Liu <imliuyzh@outlook.com>
2025-07-23 16:25:00 -0700

    Refine test cases (#2)

--- src/test/java/com/pupperfield/backend/service/TokenServiceTests.java
+++ src/test/java/com/pupperfield/backend/service/TokenServiceTests.java
@@ -2,6 +2,8 @@ package com.pupperfield.backend.service;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -20,53 +22,33 @@ public class TokenServiceTests {
         assertThat(tokenService.isValid(token)).isTrue();
     }
 
-    @Test
-    public void testInvalidToken1() {
-        assertThat(tokenService.isValid(randomUUID().toString())).isFalse();
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "",
+        "        ",
+        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE3"
+            + "NTIzOTAzMDgsImV4cCI6MTc4MzkyNjMwOCwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJv"
+            + "Y2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJ"
+            + "FbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWl"
+            + "uaXN0cmF0b3IiXX0.dghMDYh4sJaZt4lhmSPTTiiS0pjJ8gWcEPq4dHskJ4U",
+        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoibmFtZSIsImVtYWlsIjoibmFtZUBlbWFpbC5j"
+            + "b20iLCJleHAiOjE3NTIzOTQyMjIsImlhdCI6MTc1MjM5MDYyMn0.OYmZVWHyTjih8ctgAu-7PVj_bFNi"
+            + "COZth0auWPTs0J8",
+        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoibmFtZSIsImVtYWlsIjoibmFtZUBlbWFpbC5j"
+            + "b20iLCJleHAiOjE3NTIzOTQyMjIsImlhdCI6MTc1MjM5MDYyMn0.OYmZVWHyTjih8ctgAu-7PVj_bFNi"
+            + "Y2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJ"
+    })
+    public void testInvalidToken(String token) {
+        assertThat(tokenService.isValid(token)).isFalse();
     }
 
     @Test
-    public void testInvalidToken2() {
-        assertThat(tokenService.isValid("")).isFalse();
-    }
-
-    @Test
-    public void testInvalidToken3() {
-        assertThat(tokenService.isValid("        ")).isFalse();
-    }
-
-    @Test
-    public void testInvalidToken4() {
-        assertThat(tokenService.isValid(
-            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE3"
-                + "NTIzOTAzMDgsImV4cCI6MTc4MzkyNjMwOCwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoianJv"
-                + "Y2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJ"
-                + "FbWFpbCI6Impyb2NrZXRAZXhhbXBsZS5jb20iLCJSb2xlIjpbIk1hbmFnZXIiLCJQcm9qZWN0IEFkbWl"
-                + "uaXN0cmF0b3IiXX0.dghMDYh4sJaZt4lhmSPTTiiS0pjJ8gWcEPq4dHskJ4U"
-        )).isFalse();
-    }
-
-    @Test
-    public void testInvalidToken5() {
-        assertThat(tokenService.isValid(
-            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoibmFtZSIsImVtYWlsIjoibmFtZUBlbWFpbC5j"
-                + "b20iLCJleHAiOjE3NTIzOTQyMjIsImlhdCI6MTc1MjM5MDYyMn0.OYmZVWHyTjih8ctgAu-7PVj_bFNi"
-                + "COZth0auWPTs0J8"
-        )).isFalse();
-    }
-
-    @Test
-    public void testInvalidToken6() {
+    public void testNullToken() {
         assertThat(tokenService.isValid(null)).isFalse();
     }
 
     @Test
-    public void testInvalidToken7() {
-        assertThat(tokenService.isValid(
-            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoibmFtZSIsImVtYWlsIjoibmFtZUBlbWFpbC5j"
-                + "b20iLCJleHAiOjE3NTIzOTQyMjIsImlhdCI6MTc1MjM5MDYyMn0.OYmZVWHyTjih8ctgAu-7PVj_bFNi"
-                + "Y2tldEBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJSb2NrZXQiLCJ"
-                + "uaXN0cmF0b3IiXX0.dghMDYh4sJaZt4lhmSPTTiiS0pjJ8gWcEPq4dHskJ4U"
-        )).isFalse();
+    public void testUuidAsToken() {
+        assertThat(tokenService.isValid(randomUUID().toString())).isFalse();
     }
 }
