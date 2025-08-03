4a4332f1368ab469b7f3b34f4659f6710057b152
Yizhen Liu <imliuyzh@outlook.com>
2025-08-02 19:54:44 -0700

    Rename exception package to advice package

--- src/test/java/com/pupperfield/backend/advice/ExceptionAdviceIntegrationTests.java
+++ src/test/java/com/pupperfield/backend/advice/ExceptionAdviceIntegrationTests.java
@@ -1,4 +1,4 @@
-package com.pupperfield.backend.exception;
+package com.pupperfield.backend.advice;
 
 import com.pupperfield.backend.controller.AuthController;
 import com.pupperfield.backend.controller.DogController;
