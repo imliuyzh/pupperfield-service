f3138f9b9ae69ecf539208c397f8fad25535ecca
Yizhen Liu <imliuyzh@outlook.com>
2025-11-20 01:55:42 +0000

    Revise

--- src/test/java/com/pupperfield/backend/service/DogServiceCachingTests.java
+++ src/test/java/com/pupperfield/backend/service/DogServiceCachingTests.java
@@ -4,19 +4,16 @@ import com.github.benmanes.caffeine.cache.Cache;
 import com.pupperfield.backend.config.CacheConfig;
 import com.pupperfield.backend.model.DogSearchRequestDto;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cache.CacheManager;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class DogServiceCachingTests {
     @Autowired
